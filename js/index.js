0200ee48a2e622719820a8f1a022b3d0bf733b81
prenite <117414705+prenite@users.noreply.github.com>
2022-12-12 11:37:06 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,42 @@
-$(window).load(function () {
+$(document).ready(function () {
+
+
+    $('.mn_area').hide();
+    let key = true;
+    console.log(key);
+
+    $('.nav li a').mouseenter(function () {
+        if (key == true) {
+            key = false;
+            $('.mn_area').show().css({ height: 0 }).animate({ height: '230px' });
+        }
+    });
+    $('.mn_area').mouseleave(function () {
+        key = true;
+        $(this).css({ height: '230px' }).animate({ height: 0 })
+    });
+
+
+    $('#main_in .box').delay(1000).stop().animate({ top: '25vw', opacity: 0.5 });
+
 
     $(function () {
 
         $('.btn1').hide();
 
-        $('.contact img').hover(
+        $('.contact img').mouseover(
             function () {
                 $('.btn1').fadeIn();
-            }, function () {
+            }
+        )
+        $('.contact img').mouseleave(
+            function () {
                 $('.btn1').fadeOut();
             }
-        );
+        )
+
     });
-});
+
+
+
+})
